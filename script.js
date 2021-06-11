97535b1fb03b6ea297a158b5a3ca75226f109fc3
Volchenok <warp.speed@hotmail.com>
2021-06-11 01:18:57 -0700

    number counts

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 // Assignment code here
 
+const specialCharacters = "~!@#$%^&*()_-=+{}[]|\:<?/>.,";
+const generateBtn = document.getElementById('generateBtn')
+generateBtn.addEventListener("click", writePassword);
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -14,6 +15,26 @@ function writePassword() {
 
 }
 
+// When clicking button
+
+function generatePassword(){
+  var passwordLength = prompt("How many characters you want for the password? It must be between 8 and 128 characters.");
+
+  var numbers = confirm("Add numbers?");
+  var lowerCases = confirm("Add lower cases?");
+  var upperCases = confirm("Add upper cases?");
+  var special = confirm("Add special characters?");
+
+var minimumCount = 0;
+}
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 
