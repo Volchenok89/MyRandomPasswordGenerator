6ab300f957b13bc84aa84ba3c9534e90c8fc604e
Volchenok <warp.speed@hotmail.com>
2021-06-16 18:02:23 -0700

    redoing step by step

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,14 @@
-challenge 3
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~", "#", "@", "%", "\\", "[", "]", "{", "}"];
+
+function random(array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  let randomElement = array[randomIndex];
+  return randomElement;
+
+}
+
